62be39f368f3802380127221f9e5a2344d8bdbab
Elias Giliarde <contatoegiliarde@gmail.com>
2025-05-15 21:43:43 -0300

    ajustando acentos e escritas

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ function calcularIMC() {
     
     if (peso > pesoIdeal) {
         const perder = (peso - pesoIdeal).toFixed(1);
-        mensagemPesoIdeal = `Para alcançar seu peso ideal, voce pode perder cerca de ${perder} kg.`;
+        mensagemPesoIdeal = `Para alcançar seu peso ideal, você precisa perder cerca de ${perder} kg.`;
     }   else if (peso < pesoIdeal) {
         const ganhar = (peso - pesoIdeal).toFixed(1);
-        mensagemPesoIdeal = `Para alcançar seu peso ideal, voce pode ganhar cerca de ${ganhar} kg.`;
+        mensagemPesoIdeal = `Para alcançar seu peso ideal, você precisa ganhar cerca de ${ganhar} kg.`;
     }   else {
-        mensagemPesoIdeal = `Voce está com o peso ideal, mantenha assim!`
+        mensagemPesoIdeal = `Você está no seu peso ideal, mantenha-se assim!`
     }
     resultado.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> (${classificacao}).<br>${mensagemPesoIdeal}`;
 }
